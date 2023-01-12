0bd9a270545d041bc4c9dae2cf6000128a247d33
Phoeun-Chhantana <42382202+Phoeun-Chhantana@users.noreply.github.com>
2023-01-12 16:29:11 +0700

    update

--- modules/download_file.js
+++ modules/download_file.js
@@ -1,7 +1,7 @@
 async function downloadFile({url, filename}){
     //const blob = new Blob([url], { type: "octet/stream"})
     //const blob = await fetch(encodeURIComponent(url)).then((res) => res.blob()).catch((err) => alert(err))
-    const blob = new Blob([url], {type: "octet/stream"})
+    const blob = new Blob([url], {type: "video/mp4"})
     const href = window.URL.createObjectURL(blob)
     window.open(href)
     //const a = Object.assign(document.createElement("a"), {
