ad902e2d25f7ce8dab4eb7d526daea23039fa0ae
Phoeun-Chhantana <42382202+Phoeun-Chhantana@users.noreply.github.com>
2023-01-12 15:33:22 +0700

    update

--- modules/download_file.js
+++ modules/download_file.js
@@ -1,8 +1,14 @@
-'use strict'
 async function downloadFile({url, filename}){
     //const blob = new Blob([url], { type: "octet/stream"})
-    const blob = await fetch(url).then(res => res.blob())
-    const href = URL.createObjectURL(blob)
+    const options = {
+      method: "GET",
+      headers: {
+        "Accept": "application/octet-stream",
+        "Content-Type": "application/octet-stream"
+      }
+    };
+    const blob = await fetch(url, options).then(res => res.blob())
+    const href = window.URL.createObjectURL(blob)
     //const a = Object.assign(document.createElement("a"), {
       //href,
       //download: filename
@@ -15,11 +21,11 @@ async function downloadFile({url, filename}){
     const a = document.createElement("a")
     a.href = href
     a.download = filename
-    a.click()
     document.body.appendChild(a)
+    a.click()
     //const revoke = window.URL || window.URL.createObjectURL || window.webkitURL
     //revoke.revokeObjectURL(href)
-    URL.revokeObjectURL(href)
+    window.URL.revokeObjectURL(href)
     a.remove()
 
     // const a = document.createElement("a")
